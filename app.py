12832b5932ecd02b666e5f62c5694d8c1710cb8d
Антон Югов <i@ayugov.ru>
2018-09-13 01:36:14 +0300

    add traceroute

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask, request
+import subprocess
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET"])
+@app.route('/myip', methods=["GET"])
 def get_ip():
     if request.headers.getlist("X-Forwarded-For"):
         return request.headers.getlist("X-Forwarded-For")[0], 200
     else:
         return request.remote_addr, 200
+
+
+@app.route('/tracert', methods=["GET"])
+def get_traceroute():
+    if len(request.args) == 0:
+        ip = get_ip()[0]
+        traceroute = subprocess.check_output(['traceroute', ip])
+        return traceroute, 200
+    else:
+        ip = request.args.get('ip')
+        traceroute = subprocess.check_output(['traceroute', ip])
+        return traceroute, 200
 
 
 if __name__ == '__main__':
